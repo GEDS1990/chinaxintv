e83bae147c63a419697d4261eb6eb02653239022
GEDS1990 <1161865398@qq.com>
2017-09-24 12:07:26 +0800

    init by geds

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="app-footer" style="position: relative;text-align: center;">
+    <a >www.haveideal.club</a> &copy; 2017 Copyright.
+    <span class="float-right">Powered by <a href="http://www.haveideal.club">长兴网络</a></span>
+  </footer>
+</template>
+<script>
+export default {
+  name: 'footer'
+}
+</script>
